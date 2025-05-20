532265e269dbe014b105cc0e3edd08c53ba6123b
Summer2202 <evashao2020@gmail.com>
2025-05-20 19:53:24 +1200

    Create Starter: Biggest, Smallest and Average.py

---
+++ Starter: Biggest, Smallest and Average.py
@@ -0,0 +1,12 @@
+numsList = [7, 6, 23, 8.18, 18, 8, 7.2, 85, 915, 12]
+largest=max(numsList)
+print(largest)
+smallest=min(numsList)
+print(smallest)
+Largest=915
+position=numsList.index(Largest)
+print(position+1)
+smallest=6
+position2=numsList.index(smallest)
+print(position2+1)
+print((7+6+23+8.28+18+8+7.2+85+915+12)/10)
